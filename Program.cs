19a3c07da15702c2333bd945b5ee76cbe90b0434
shadowenlight <isikomer98@gmail.com>
2023-11-09 00:11:01 +0300

    ReadtxtFile

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WordFrequency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:\\Users\\isiko\\source\\repos\\ReadtxtFile\\txt\\TextFile1.txt";
+            string text = "";
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                text = reader.ReadToEnd();
+            }
+
+            Regex regex = new Regex(@"\b\p{L}+\b", RegexOptions.IgnoreCase);
+            MatchCollection matches = regex.Matches(text);
+
+            string[] words = matches.Cast<Match>().Select(match => match.Value.ToLower()).ToArray();
+
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+            foreach (string word in words)
+            {
+                if (wordCounts.ContainsKey(word))
+                    wordCounts[word]++;
+                else
+                    wordCounts.Add(word, 1);
+            }
+
+            var topWords = wordCounts.OrderByDescending(pair => pair.Value).Take(10);
+
+            Console.WriteLine("Top 10 most frequent words:");
+            foreach (var pair in topWords)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
+        }
+    }
+}
